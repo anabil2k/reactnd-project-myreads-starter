e9352ea46f0474f6c6663f2dd18bddad4ed4f06b
Ahmed Nabil work <a_nabil2k@yahoo.com>
2021-05-06 22:51:22 +0200

    Component separation

---
+++ src/components/SearchComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+/*  class SearchComp extends React.Componenet{
+    render()*/
+    function SearchComp(){
+        return(
+<ol className="books-grid">
+   
+</ol>
+)}
+export default SearchComp
